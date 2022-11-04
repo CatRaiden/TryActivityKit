53591ce049ca527ab02cbbcef327116d3f6b62e1
Kevin <acxzop11@gmail.com>
2022-11-04 17:47:58 +0800

    Initial Commit

---
+++ TryActivityKit/TryActivityKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryActivityKitApp.swift
+//  TryActivityKit
+//
+//  Created by Kevin on 2022/11/4.
+//
+
+import SwiftUI
+
+@main
+struct TryActivityKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
